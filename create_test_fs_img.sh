a6b04c15cb5b3f52c61e2becbb2f02625229f8e4
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-07-20 17:55:51 +0800

    Add script for creating virtio-blk test image

---
+++ create_test_fs_img.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DISK_IMG_SIZE=2k
+DISK_IMG_NAME=ext4.img
+
+dd if=/dev/zero of=$DISK_IMG_NAME bs=4k count=$DISK_IMG_SIZE
+mkfs.ext4 $DISK_IMG_NAME
+tune2fs -c0 -i0 $DISK_IMG_NAME
+mkdir -p mnt/
+sudo mount ext4.img mnt/
+sudo sh -c "echo \"Hello World!\" > mnt/hello.txt"
+sudo umount mnt/
