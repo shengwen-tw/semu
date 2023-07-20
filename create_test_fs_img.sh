ef6a8e6263c774e4dc8b2e579c1f1f440b484126
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-07-20 17:59:11 +0800

    Fix hard-coded disk image name

--- create_test_fs_img.sh
+++ create_test_fs_img.sh
@@ -7,6 +7,6 @@ dd if=/dev/zero of=$DISK_IMG_NAME bs=4k count=$DISK_IMG_SIZE
 mkfs.ext4 $DISK_IMG_NAME
 tune2fs -c0 -i0 $DISK_IMG_NAME
 mkdir -p mnt/
-sudo mount ext4.img mnt/
+sudo mount $DISK_IMG_NAME mnt/
 sudo sh -c "echo \"Hello World!\" > mnt/hello.txt"
 sudo umount mnt/
