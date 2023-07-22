9f3b6ad30b966dcfc05c5617470f1df1653cdcf9
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-07-22 17:30:43 +0800

    Create `virtio.h` for common code definitions of the virtio devices

--- virtio-blk.c
+++ virtio-blk.c
@@ -15,19 +15,9 @@
 #include "device.h"
 #include "riscv.h"
 #include "riscv_private.h"
+#include "virtio.h"
 
 #define DISK_BLK_SIZE 512
-
-#define VIRTIO_VENDOR_ID 0x12345678
-
-#define VIRTIO_STATUS__DRIVER_OK 4
-#define VIRTIO_STATUS__DEVICE_NEEDS_RESET 64
-
-#define VIRTIO_INT__USED_RING 1
-#define VIRTIO_INT__CONF_CHANGE 2
-
-#define VIRTIO_DESC_F_NEXT 1
-#define VIRTIO_DESC_F_WRITE 2
 
 #define VBLK_FEATURES_0 0
 #define VBLK_FEATURES_1 1 /* VIRTIO_F_VERSION_1 */
@@ -37,26 +27,6 @@
 #define VBLK_PREPROCESS_ADDR(addr)                        \
     ((addr) < RAM_SIZE && !((addr) &0b11) ? ((addr) >> 2) \
                                           : (virtio_blk_set_fail(vblk), 0))
-
-#define VIRTIO_BLK_T_IN 0
-#define VIRTIO_BLK_T_OUT 1
-#define VIRTIO_BLK_T_FLUSH 4
-#define VIRTIO_BLK_T_GET_ID 8
-#define VIRTIO_BLK_T_GET_LIFETIME 10
-#define VIRTIO_BLK_T_DISCARD 11
-#define VIRTIO_BLK_T_WRITE_ZEROES 13
-#define VIRTIO_BLK_T_SECURE_ERASE 14
-
-#define VIRTIO_BLK_S_OK 0
-#define VIRTIO_BLK_S_IOERR 1
-#define VIRTIO_BLK_S_UNSUPP 2
-
-struct virtq_desc {
-    uint32_t addr;
-    uint32_t len;
-    uint16_t flags;
-    uint16_t next;
-};
 
 struct vblk_req_hdr {
     uint32_t type;
